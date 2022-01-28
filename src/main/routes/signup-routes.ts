95445fb04979309f0c4085448258a204ade3ff94
Ramon Nunes <ramonjs.dev@gmail.com>
2022-01-28 14:11:58 -0300

    Login concluído

--- src/main/routes/signup-routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { makeSignUpController } from '../factories/login/signup/signup-factory'
-import { adaptRoute } from '../adapters/express/express-routes-adapter'
-
-
-export default (router: Router): void => {
-  router.post('/signup', adaptRoute(makeSignUpController()))
-}
